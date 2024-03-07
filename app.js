fff0c2d4d0a2294031ff46eeb62e3a36844509be
gaurishgarg <gaurish_garg@outlook.com>
2024-03-07 16:43:02 +0530

    test

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
 const express = require('express');
-
+const axios = require('axios');
+const cheerio = require('cheerio');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: true }));
 let postData = {}; // Variable to store the POST data
 
-const puppeteer = require('puppeteer');
 
-async function runStreamlitHeadlessly() {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto('https://projectbase-gaurish.streamlit.app/');
-      // Extract JSON data from the Streamlit UI
-    // Extract information from the Streamlit UI
-    const pageContent = await page.content();
 
-    // Extract JSON data from the page content
-    console.log(pageContent)
+async function scrapeDataFromWebPage() {
+    try {
+        const response = await axios.get('https://example.com/page');
+        console.log(esponse.data);
+        const $ = cheerio.load(response.data);
+        // Use Cheerio selectors to extract data from the page
+        // const data = $('selector').text();
+        postData = {"demo":"Demo"};
+    } catch (error) {
+        console.error('Error scraping data:', error);
+    }
 }
 
 
-app.get("/connectme",async function(req,res){
-    await runStreamlitHeadlessly();
+
+app.get("/connectme", async function (req, res) {
+    await scrapeDataFromWebPage();
     res.send(postData);
 });
 
-
-app.post('getdata', function(req,res){
+app.post('/getdata', function (req, res) {
     console.log('Received data:', req.body);
     postData = req.body;
     // Respond to the client
     res.status(200).json({ message: 'POST request received successfully' });
-})
-
-
-
+});
 
 module.exports = app;

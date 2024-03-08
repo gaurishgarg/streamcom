bf73275c89fc1e8cbf95faea5416308eca917981
gaurishgarg <gaurish_garg@outlook.com>
2024-03-08 19:15:07 +0530

    seendport

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ require("dotenv").config();
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // Use JSON body parser middleware
-
-app.post('/getdata', function(req, res) {
-  console.log("Data Received:", req.body);
-  res.sendStatus(200); // Send a 200 status code
-});
+var instances = new List();
 app.get("/", function(req,res){
   res.send("This is the face of backend");
 })
+
+app.post("/whatsmyport", function(req,res){
+  let received_browserid = req.body.browserid;
+  waitForPostDataPromise = new Promise(resolve => {
+    let timeout = setTimeout(() => {
+      reject("Handshake refused");
+  }, 120000); 
+    app.post('/getdata', (myreq, myres) => {
+        const postData = myreq.body;
+        if(received_browserid == postData.browserid){
+          myres.status(200).json({ message: 'POST request received successfully' });
+          resolve(postData);
+        }     
+    });
+   });
+   console.log("Promise resolved");
+   console.log(waitForPostDataPromise);
+   instances.add({"browserid": received_browserid, "data_bind": waitForPostDataPromise});
+   res.send(waitForPostDataPromise);
+});
 module.exports = app;

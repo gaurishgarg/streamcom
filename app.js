c0d6eed7852ab099b41867b6e099639b8a12cd68
gaurishgarg <gaurish_garg@outlook.com>
2024-03-08 19:34:38 +0530

    debug

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser'); // Import body-parser middleware
 const express = require('express');
 const List = require('collections/list');
+const { rejects } = require('assert');
 require("dotenv").config();
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -12,14 +13,22 @@ app.get("/", function(req,res){
 
 app.post("/whatsmyport", function(req,res){
   let received_browserid = req.body.browserid;
-  waitForPostDataPromise = new Promise(resolve => {
+  console.log("Received browserid");
+  console.log(received_browserid);
+  waitForPostDataPromise = new Promise(resolve,reject => {
+    console.log("In promise, waiting for post request");
     let timeout = setTimeout(() => {
       reject("Handshake refused");
   }, 120000); 
     app.post('/getdata', (myreq, myres) => {
-        const postData = myreq.body;
+        console.log("a post request received, let me check it");
+        let postData = myreq.body;
+        console.log("I received this post request");
+        console.log(postData)
         if(received_browserid == postData.browserid){
+          console.log("Browser ID Matched, Sending 200 OK");
           myres.status(200).json({ message: 'POST request received successfully' });
+          console.log("Browser ID Matched, Resolving promise");
           resolve(postData);
         }     
     });

634598d208f475cf313b023e7a7a0d18dac3e6d1
gaurishgarg <gaurish_garg@outlook.com>
2024-03-08 17:48:11 +0530

    post

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // Use JSON body parser middleware
 
-
-app.post('/getdata', function(req,res){
-  console.log("Data Received: "+req.body);
-  res.statusCode = 200;
+app.post('/getdata', function(req, res) {
+  console.log("Data Received:", req.body);
+  res.sendStatus(200); // Send a 200 status code
 });
-
-
+app.get("/", function(req,res){
+  res.send("This is the face of backend");
+})
 module.exports = app;
